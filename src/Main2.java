199b60bb098d9240fcec2bf62611c4ef179ecde6
EdreesShinwari <ekshinwari@gmail.com>
2020-11-12 11:22:47 -0500

    Made commit so we can see our history

--- src/Main2.java
+++ src/Main2.java
@@ -1,2 +1,6 @@
 public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("Everybody wants to rule the world");
+        //B21 is learning java, soft skills and now auto testing
+    }
 }
